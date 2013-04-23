ec77413913b209a9c1d39d137ad8b5578185ce8c
wuliwong <patrickbradley777@gmail.com>
2013-04-23 16:30:19 -0400

    first commit initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bitsandgold::Application.config.secret_token = '16a515a6572ccba3cfda576bce286012ce811460613d66d0d4a17b9966037783e08f967ef3de5d3fda2aa8e53ddfb1dda749c3b0bc0bbc3c696cfec4beddb8dc'
