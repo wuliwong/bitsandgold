e75b3c5399f9e86cdcae07395b18bc9602816855
wuliwong <patrickbradley777@gmail.com>
2013-04-23 17:54:43 -0400

    fully functioning page which you can look up a symbol and have it priced in gold, silver and bitcoin

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,8 +6,20 @@ class HomeController < ApplicationController
   base_uri "api.bitcoincharts.com"
 
   def index
-    @price = MarketBeat.opening_price :AAPL
-    @bitcoin_json = HomeController.get "http://appsrv.cse.cuhk.edu.hk/~rysun/goldprice/"
-    @bitcoin_json = JSON.parse @bitcoin_json.parsed_response
+    if params[:symbol].nil?
+      @symbol = "AAPL"
+    else
+      @symbol = params[:symbol]
+    end
+    @price = MarketBeat.opening_price @symbol.to_sym
+    #@metal_result = HomeController.get "http://appsrv.cse.cuhk.edu.hk/~rysun/goldprice/"
+    #@metal_json = JSON.parse @metal_result.parsed_response
+    @bitcoin_result = HomeController.get "http://data.mtgox.com/api/1/BTCUSD/ticker"
+    @bitcoin_json = @bitcoin_result.parsed_response
+    @coinbul_result = HomeController.get "http://coinabul.com/api.php"
+    @coinbul_json = JSON.parse @coinbul_result.parsed_response
+    @gold_price = @coinbul_json["Gold"]["USD"]
+    @silver_price = @coinbul_json["Silver"]["USD"]
+    @btc_price = @coinbul_json["BTC"]["USD"]
   end
 end
