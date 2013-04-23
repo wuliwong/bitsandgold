8a28933d2f79ba39d6149d1573834b55c84fb44d
wuliwong <patrickbradley777@gmail.com>
2013-04-23 17:05:30 -0400

    added some initial test price retrieval and recalculation

--- Gemfile
+++ Gemfile
@@ -22,6 +22,7 @@ end
 
 gem 'jquery-rails'
 
+gem 'httparty'
 gem 'market_beat'
 
 # To use ActiveModel has_secure_password
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,13 @@
+require "market_beat"
+require "httparty"
+
 class HomeController < ApplicationController
+  include HTTParty
+  base_uri "api.bitcoincharts.com"
+
   def index
+    @price = MarketBeat.opening_price :AAPL
+    @bitcoin_json = HomeController.get "http://appsrv.cse.cuhk.edu.hk/~rysun/goldprice/"
+    @bitcoin_json = JSON.parse @bitcoin_json.parsed_response
   end
 end
